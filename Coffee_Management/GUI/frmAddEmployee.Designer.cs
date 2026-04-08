ec05b250de53497f2173107dd389eb0b7b76bba5
Huy99-gif <ga644376@gmail.com>
2026-04-08 12:36:13 +0700

    Design

--- Coffee_Management/GUI/frmAddEmployee.Designer.cs
+++ Coffee_Management/GUI/frmAddEmployee.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddEmployee));
             txtPassword = new TextBox();
             btnSave = new Button();
             dtpHireDate = new DateTimePicker();
@@ -38,106 +39,136 @@
             txtEmpID = new TextBox();
             btnCancel = new Button();
             textBox1 = new TextBox();
+            panel1 = new Panel();
             SuspendLayout();
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(328, 408);
+            txtPassword.Location = new Point(369, 469);
+            txtPassword.MinimumSize = new Size(280, 0);
             txtPassword.Name = "txtPassword";
             txtPassword.PlaceholderText = "Password";
-            txtPassword.Size = new Size(224, 27);
+            txtPassword.Size = new Size(280, 30);
             txtPassword.TabIndex = 17;
             // 
             // btnSave
             // 
+            btnSave.BackColor = Color.FromArgb(52, 152, 219);
+            btnSave.FlatAppearance.BorderSize = 0;
+            btnSave.FlatStyle = FlatStyle.Flat;
             btnSave.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnSave.Location = new Point(192, 464);
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(296, 544);
             btnSave.Name = "btnSave";
-            btnSave.Size = new Size(120, 56);
+            btnSave.Size = new Size(120, 40);
             btnSave.TabIndex = 15;
             btnSave.Text = "Save";
-            btnSave.UseVisualStyleBackColor = true;
+            btnSave.UseVisualStyleBackColor = false;
             btnSave.Click += btnSave_Click;
             // 
             // dtpHireDate
             // 
-            dtpHireDate.Location = new Point(328, 264);
+            dtpHireDate.Location = new Point(369, 304);
+            dtpHireDate.MinimumSize = new Size(280, 0);
             dtpHireDate.Name = "dtpHireDate";
-            dtpHireDate.Size = new Size(224, 27);
+            dtpHireDate.Size = new Size(280, 30);
             dtpHireDate.TabIndex = 14;
             // 
             // cboRole
             // 
             cboRole.FormattingEnabled = true;
             cboRole.Items.AddRange(new object[] { "manager", "staff" });
-            cboRole.Location = new Point(328, 360);
+            cboRole.Location = new Point(369, 414);
+            cboRole.MinimumSize = new Size(280, 0);
             cboRole.Name = "cboRole";
-            cboRole.Size = new Size(151, 28);
+            cboRole.Size = new Size(280, 31);
             cboRole.TabIndex = 13;
             cboRole.Text = "Vai trò";
             // 
             // txtPhone
             // 
-            txtPhone.Location = new Point(328, 312);
+            txtPhone.Location = new Point(369, 359);
+            txtPhone.MinimumSize = new Size(280, 0);
             txtPhone.Name = "txtPhone";
             txtPhone.PlaceholderText = "Phone Number";
-            txtPhone.Size = new Size(216, 27);
+            txtPhone.Size = new Size(280, 30);
             txtPhone.TabIndex = 12;
             // 
             // txtEmail
             // 
-            txtEmail.ForeColor = SystemColors.HighlightText;
-            txtEmail.Location = new Point(328, 168);
+            txtEmail.BorderStyle = BorderStyle.FixedSingle;
+            txtEmail.ForeColor = Color.FromArgb(70, 70, 70);
+            txtEmail.Location = new Point(369, 193);
+            txtEmail.MinimumSize = new Size(280, 0);
             txtEmail.Name = "txtEmail";
             txtEmail.PlaceholderText = "Email";
-            txtEmail.Size = new Size(240, 27);
+            txtEmail.Size = new Size(280, 30);
             txtEmail.TabIndex = 11;
             // 
             // txtFullName
             // 
-            txtFullName.Location = new Point(328, 216);
+            txtFullName.BorderStyle = BorderStyle.FixedSingle;
+            txtFullName.ForeColor = Color.FromArgb(70, 70, 70);
+            txtFullName.Location = new Point(369, 248);
+            txtFullName.MinimumSize = new Size(280, 0);
             txtFullName.Name = "txtFullName";
             txtFullName.PlaceholderText = "FullName";
-            txtFullName.Size = new Size(248, 27);
+            txtFullName.Size = new Size(280, 30);
             txtFullName.TabIndex = 10;
             // 
             // txtEmpID
             // 
-            txtEmpID.Location = new Point(328, 120);
+            txtEmpID.BorderStyle = BorderStyle.FixedSingle;
+            txtEmpID.Location = new Point(369, 138);
+            txtEmpID.MinimumSize = new Size(280, 0);
             txtEmpID.Name = "txtEmpID";
             txtEmpID.ReadOnly = true;
-            txtEmpID.Size = new Size(128, 27);
+            txtEmpID.Size = new Size(280, 30);
             txtEmpID.TabIndex = 9;
             // 
             // btnCancel
             // 
+            btnCancel.BackColor = Color.FromArgb(189, 195, 199);
+            btnCancel.FlatAppearance.BorderSize = 0;
+            btnCancel.FlatStyle = FlatStyle.Flat;
             btnCancel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnCancel.Location = new Point(576, 464);
+            btnCancel.ForeColor = Color.Black;
+            btnCancel.Location = new Point(624, 544);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(128, 56);
+            btnCancel.Size = new Size(120, 40);
             btnCancel.TabIndex = 18;
             btnCancel.Text = "Cancel";
-            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.UseVisualStyleBackColor = false;
             btnCancel.Click += bttCancel_Click;
             // 
             // textBox1
             // 
             textBox1.BackColor = SystemColors.HighlightText;
-            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.ForeColor = SystemColors.InactiveCaptionText;
-            textBox1.Location = new Point(296, 40);
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.FromArgb(44, 62, 80);
+            textBox1.Location = new Point(333, 46);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(328, 43);
+            textBox1.Size = new Size(368, 47);
             textBox1.TabIndex = 19;
             textBox1.Text = "Tạo tài khoản";
             textBox1.TextAlign = HorizontalAlignment.Center;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Location = new Point(296, 24);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(448, 500);
+            panel1.TabIndex = 20;
+            // 
             // frmAddEmployee
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(45, 45, 48);
-            ClientSize = new Size(969, 575);
+            BackColor = SystemColors.ButtonFace;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1090, 661);
             Controls.Add(textBox1);
             Controls.Add(btnCancel);
             Controls.Add(txtPassword);
@@ -148,6 +179,8 @@
             Controls.Add(txtEmail);
             Controls.Add(txtFullName);
             Controls.Add(txtEmpID);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Name = "frmAddEmployee";
             Text = "frmAddEmployee";
             ResumeLayout(false);
@@ -166,5 +199,6 @@
         private TextBox txtEmpID;
         private Button btnCancel;
         private TextBox textBox1;
+        private Panel panel1;
     }
 }
